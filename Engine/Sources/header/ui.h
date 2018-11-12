4b0aec45e94ff1650152df0e3e537f5109527909
batburger <a_modenbach@outlook.com>
2018-11-12 19:09:20 +0100

    "Fixed" memory leak.

--- Engine/Sources/header/ui.h
+++ Engine/Sources/header/ui.h
@@ -5,7 +5,7 @@
 namespace Simple2D{
     
     namespace Ui {
-        void createText(std::string text, vec3 pos, vec3 scale = vec3(0.1, 0.1, 0.1), float characterGap = 1.5f);
+        void createText(std::string text, vec3 pos, vec3 scale = vec3(0.1, 0.1, 0.1), float characterGap = 0.4f);
         void changeTextColour(std::string fontName, unsigned char red, unsigned char green, unsigned char blue);
     }
 }
